992bbaea8e9d081f5117bced335c90e3212df455
Matt Mundell <matt@mundell.me>
2024-11-21 14:03:52 +0200

    Export a LanguageSupport instead of a ViewPlugin

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { ViewPlugin } from '@codemirror/view';
-import { syntaxHighlighting } from '@codemirror/language';
+import { syntaxHighlighting, LanguageSupport } from '@codemirror/language';
 import { markdown } from '@codemirror/lang-markdown';
 
 import tagParser from './tagParser';
@@ -11,24 +11,27 @@ import type { Config } from '@markdoc/markdoc';
 
 export type MarkdocPluginConfig = { lezer?: any, markdoc: Config };
 
-export default function (config: MarkdocPluginConfig) {
-  const mergedConfig = {
-    ...config.lezer ?? [],
-    extensions: [tagParser, ...config.lezer?.extensions ?? []]
-  };
-
-  return ViewPlugin.fromClass(RichEditPlugin, {
-    decorations: v => v.decorations,
-    provide: v => [
-      renderBlock(config.markdoc),
-      syntaxHighlighting(highlightStyle),
-      markdown(mergedConfig)
-    ],
-    eventHandlers: {
-      mousedown({ target }, view) {
-        if (target instanceof Element && target.matches('.cm-markdoc-renderBlock *'))
-          view.dispatch({ selection: { anchor: view.posAtDOM(target) } });
+export
+function richdoc (config: MarkdocPluginConfig) {
+  const plugin = ViewPlugin.fromClass(RichEditPlugin, {
+      decorations: v => v.decorations,
+      provide: v => [
+        renderBlock(config?.markdoc ?? {}),
+        syntaxHighlighting(highlightStyle)
+      ],
+      eventHandlers: {
+        mousedown({ target }, view) {
+          if (target instanceof Element && target.matches('.cm-markdoc-renderBlock *'))
+            view.dispatch({ selection: { anchor: view.posAtDOM(target) } });
+        }
       }
-    }
-  });
+    })
+  const mergedConfig = {
+    ...config?.lezer ?? [],
+    extensions: [tagParser, ...config?.lezer?.extensions ?? []]
+  };
+  const ls = markdown(mergedConfig);
+  ls.language.name = 'richdoc'
+
+  return new LanguageSupport(ls, plugin);
 }
